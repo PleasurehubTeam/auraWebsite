39c5173a8af89c25363e0d8bb7c9c3363ad6a51a
lee0821 <lee0821@yeah.net>
2026-05-24 01:38:11 +0800

    feat: app1.3.5 版本安卓包地址更新

--- src/config/download.ts
+++ src/config/download.ts
@@ -6,7 +6,7 @@ export const downloadConfig: DownloadConfig = {
   googlePlayUrl:
     "https://play.google.com/store/apps/details?id=com.echoai.aura.app&pcampaignid=web_share",
   androidApkUrl:
-    "https://assets.pleasurehub.app/apk-updates/app_2.0.1_10220.apk",
+    "https://assets.pleasurehub.app/apk-updates/app_1.3.5_10305.apk",
   heading: "Aura",
   description: "Experience AI-powered intelligent sensory technology.",
 };
